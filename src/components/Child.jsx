a06b9a4e2a65b8b3189ee1422cca632187ba650d
18subrato <subratoverma7@gmail.com>
2025-04-02 21:28:18 +0530

    initial commit

---
+++ src/components/Child.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Child = ({ showModal, setShowModal }) => {
+    return (
+        <div style={{backgroundColor:"darkred"}}>
+            <h2>Child Component</h2>
+            <button onClick={() => setShowModal(true)}>Show Modal</button>
+            {
+                showModal && (
+                    <div>
+                        <h3>Modal Content</h3>
+                        <p>This is the modal content.</p>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default Child
